ef658cd3b6638ccd59885de51859815c049ce53e
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2021-10-02 15:55:50 -0600

    Challenge Wikinimous

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(params_article)
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def params_article
+    params.require(:article).permit(:title, :content)
+  end
+end
